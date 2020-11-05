b360e563f93704cb5c79cd4077dd4da02edb734f
Minh Tri Ho <b3r0ngheroq@gmail.com>
2020-11-05 21:05:54 +0700

    hello test tai file len cho vui

---
+++ mycrush.py
@@ -0,0 +1,14 @@
+list_crush =('Ánh minh','Như Thư',"Hiền")
+def chao(ten):
+     """Hàm này dùng để
+     chào một người được truyền
+     vào như một tham số""" 
+     print("Xin chào, " + ten + "!")
+     return
+chao ("những bạn đang ngồi đây")     
+chao("minh trí hồ")
+chao('nguyễn tiến thịnh')
+for crush in list_crush:
+          chao(crush)
+          
+     
