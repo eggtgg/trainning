9c5bb8f7611d92287cc8097fa01ad0d26bfab339
Minh Tri Ho <b3r0ngheroq@gmail.com>
2020-11-05 21:15:39 +0700

    how are you today?

---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+
+class K1_KHDL_TTNT(unittest.TestCase):
+    def test_a(self):
+        self.assertEqual('a'*6,'aaaaaa')
+        pass
+    def test_b(bta):
+        pass
+    def test_tenThiSinh(self):
+        self.assertEqual(0,0)
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
